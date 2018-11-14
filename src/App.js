e735a38f6481d2951bd570e864171e3ac54cd3a6
Thomas TSE <chun_hei.tse@etu.uca.fr>
2018-11-14 13:20:49 +0100

    knit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopNav from './component/TopNav.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <TopNav />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
